8d84b92dd4a5e68034d21a66b1f34e2cfaffcf38
budarazvancristian <budarazvancristian@gmail.com>
2024-10-01 20:29:59 +0300

    Implement AddFoodButton

--- src/components/FoodEntryCard.jsx
+++ src/components/FoodEntryCard.jsx
@@ -1,25 +1,6 @@
 import "./styling/foodEntryCard.scss";
-import { useState, useEffect } from "react";
 
 export default function FoodEntryCard() {
-  const [data, setData] = useState([]);
-
-  const getData = () => {
-    fetch("public/listOfFood.json", {
-      method: "GET",
-    })
-      .then((response) => response.json()) // Transformă răspunsul în format JSON
-      .then((data) => {
-        setData(data); // Setează starea cu datele primite
-        console.log(data); // Opțional: Verifică datele în consola browserului
-      })
-      .catch((error) => console.log(error)); // Afișează erorile, dacă există
-  };
-
-  useEffect(() => {
-    getData(); // Apelează funcția de fetch la montarea componentului
-  }, []);
-
   return (
     <div className="food-card-container">
       <div className="header-card-container">
@@ -54,21 +35,6 @@ export default function FoodEntryCard() {
           <hr className="container-hr-calories" />
           <button className="center-button">Add More</button>
         </div>
-        {/* <div className="foodFetch">
-          <ul>
-            {data.map((food) => (
-              <div key={food.id} className="foodItem">
-                <div className="foodDetails">
-                  <img src={food.image} alt={food.name} width="100" />
-                  <div className="foodInfo">
-                    <h3>{food.name}</h3>
-                    <p>{food.calories}</p>
-                  </div>
-                </div>
-              </div>
-            ))}
-          </ul>
-        </div> */}
       </div>
       <div className="lunch-card">
         <div className="card-details">
